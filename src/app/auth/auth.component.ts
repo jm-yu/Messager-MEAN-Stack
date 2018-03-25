388a7fa4be476aef368ba38ca5755de655ce3463
jmyu1994 <jmyu1994@gmail.com>
2018-03-25 15:41:50 -0400

    add a missing file

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  template: `
+    <!--<header class="row spacing">-->
+      <!--<nav class="col-md-8 col-md-offset-2">-->
+        <!--<&#45;&#45;<ul class="nav nav-tabs">-->
+          <!--<li routerLinkActive="active"><a [routerLink]="['signup']">Signup</a></li>-->
+          <!--<li routerLinkActive="active"><a [routerLink]="['signin']">Signin</a></li>-->
+          <!--<li routerLinkActive="active"><a [routerLink]="['logout']">Logout</a></li>-->
+        <!--</ul>-->
+      <!--</nav>-->
+    <!--</header>-->
+    <!--<div class="row spacing">-->
+      <!--<router-outlet></router-outlet>-->
+    <!--</div>-->
+    <p class="row">auth works</p>
+    `
+})
+export class AuthComponent{
+
+}
