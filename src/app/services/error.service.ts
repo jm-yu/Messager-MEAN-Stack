3e505fb9c9d3d80216f1294ed8dbbc681973a38b
jmyu1994 <jmyu1994@gmail.com>
2018-04-04 00:50:37 -0400

    errors

--- src/app/services/error.service.ts
+++ src/app/services/error.service.ts
@@ -5,12 +5,11 @@ import { Error } from '../models/error.model'
 @Injectable()
 
 export class ErrorService{
-  errorOccured = new EventEmitter<Error>();
+  errorOccurred = new EventEmitter<Error>();
+
   handleError(error: any){
-    console.log(error);
-    console.log(error.title);
-    console.log(error.error.message);
+
     const errorData = new Error(error.title, error.error.message);
-    this.errorOccured.emit(errorData);
+    this.errorOccurred.emit(errorData);
   }
 }
